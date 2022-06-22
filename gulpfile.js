31cfd487fefcc32af1e53161ceac0d9c55b5f49b
Zachary Meyer <absencelul@gmail.com>
2022-06-21 21:06:37 -0700

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-  return src('scss/nova.scss')
+  return src('scss/*.scss')
     .pipe(sass())
     .pipe(dest('dist'))
 }
 
 function watchTask() {
-  watch(['scss/nova.scss'], buildStyles)
+  watch(['scss/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
