185ca965b6d9c4526793f5d50c2bd515209cf0ac
Zachary Meyer <absencelul@gmail.com>
2022-06-21 21:25:22 -0700

    feat: added prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # nova
+
 Nova is a frontend CSS framework for developing modern and responsive web UI's.
 
 This project is currently a WIP.
--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-  return src('scss/*.scss')
-    .pipe(sass())
-    .pipe(dest('dist'))
+  return src('scss/*.scss').pipe(sass()).pipe(dest('dist'))
 }
 
 function watchTask() {
